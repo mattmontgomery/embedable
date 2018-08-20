0a7c18bf95d1b37939e2250828946b84541ba47e
Matt Montgomery <montgomery.matt@gmail.com>
2018-08-20 10:42:43 -0600

    Updates deps and fixes lodash breakage

--- test/index.js
+++ test/index.js
@@ -1,58 +1,53 @@
-var chai = require("chai")
-  , assert = chai.assert
-  , chaiAsPromised = require("chai-as-promised")
-  , Embedable = require('../index.js');
+var chai = require("chai"),
+  assert = chai.assert,
+  chaiAsPromised = require("chai-as-promised"),
+  Embedable = require("../index.js");
 
 chai.use(chaiAsPromised);
 
 var embed = Embedable();
 
-describe('Embedable Module', function() {
-
-  it('Is a constructor', function() {
-    assert.typeOf(Embedable, 'function');
+describe("Embedable Module", function() {
+  it("Is a constructor", function() {
+    assert.typeOf(Embedable, "function");
   });
 
-  it('Has a provider constructor', function() {
-    assert.typeOf(Embedable.Provider, 'function');
+  it("Has a provider constructor", function() {
+    assert.typeOf(Embedable.Provider, "function");
   });
 
-  it('Has a providers array', function() {
-    assert.typeOf(Embedable.Providers, 'array');
+  it("Has a providers array", function() {
+    assert.typeOf(Embedable.Providers, "array");
   });
 
-  it('Has more than 80 providers', function() {
+  it("Has more than 80 providers", function() {
     assert.isAtLeast(Embedable.Providers.length, 80);
   });
-
 });
 
-describe('Instance Methods', function() {
-
-  it('Has addProvider()', function() {
-    assert.typeOf(embed.addProvider, 'function');
+describe("Instance Methods", function() {
+  it("Has addProvider()", function() {
+    assert.typeOf(embed.addProvider, "function");
   });
 
-  it('Has findProvider()', function() {
-    assert.typeOf(embed.findProvider, 'function');
+  it("Has findProvider()", function() {
+    assert.typeOf(embed.findProvider, "function");
   });
 
-  it('Has fetchProvider()', function() {
-    assert.typeOf(embed.fetchProvider, 'function');
+  it("Has fetchProvider()", function() {
+    assert.typeOf(embed.fetchProvider, "function");
   });
 
-  it('Has fetch()', function() {
-    assert.typeOf(embed.fetch, 'function');
+  it("Has fetch()", function() {
+    assert.typeOf(embed.fetch, "function");
   });
 
-  it('Has render()', function() {
-    assert.typeOf(embed.render, 'function');
+  it("Has render()", function() {
+    assert.typeOf(embed.render, "function");
   });
-
 });
 
-describe('Fetch Method', function() {
-
+describe("Fetch Method", function() {
   //  it('Is able to fetch from imgur', function() {
   //   return embed.fetch('http://imgur.com/gallery/wz49SF8')
   //   .then(function(data) {
@@ -66,44 +61,62 @@ describe('Fetch Method', function() {
   //   });
   // });
 
-  it('Is able to fetch from vimeo', function() {
-    return embed.fetch('https://vine.co/v/5AvZeItT92Z')
-    .then(function(data) {
-      console.log(data);
-      console.log("----------------------------------------");
-      return embed.render(data);
-    })
-    .then(function(html) {
-      console.log(html);
-      assert.typeOf(html, 'string');
-    });
+  it("Is able to fetch from vimeo", function() {
+    return embed
+      .fetch("https://vine.co/v/5AvZeItT92Z")
+      .then(function(data) {
+        console.log(data);
+        console.log("----------------------------------------");
+        return embed.render(data);
+      })
+      .then(function(html) {
+        console.log(html);
+        assert.typeOf(html, "string");
+      });
   });
 
-  it('Is able to fetch from qzzr', function() {
-    return embed.fetch('https://www.qzzr.com/c/quiz/253072/which-dog-are-you-45cc751a-48dc-46df-a327-6703b44a14a5')
-    .then(function(data) {
-      console.log(data);
-      console.log("----------------------------------------");
-      return embed.render(data);
-    })
-    .then(function(html) {
-      console.log(html);
-      assert.typeOf(html, 'string');
-    });
+  // it("Is able to fetch from qzzr", function() {
+  //   return embed
+  //     .fetch(
+  //       "https://www.qzzr.com/c/quiz/253072/which-dog-are-you-45cc751a-48dc-46df-a327-6703b44a14a5"
+  //     )
+  //     .then(function(data) {
+  //       console.log(data);
+  //       console.log("----------------------------------------");
+  //       return embed.render(data);
+  //     })
+  //     .then(function(html) {
+  //       console.log(html);
+  //       assert.typeOf(html, "string");
+  //     });
+  // });
+  it("Is able to fetch from twitter", function() {
+    return embed
+      .fetch("https://twitter.com/slcmayor/status/1022618019904729088")
+      .then(function(data) {
+        console.log(data);
+        console.log("----------------------------------------");
+        return embed.render(data);
+      })
+      .then(function(html) {
+        console.log(html);
+        assert.typeOf(html, "string");
+      });
   });
 
-  it('Is able to fetch from pollcaster', function() {
-    return embed.fetch('https://www.pollcaster.com/c/poll/4xi4zsfv5gSXhnkaVoT9pfTOOgm')
-    .then(function(data) {
-      console.log(data);
-      console.log("----------------------------------------");
-      return embed.render(data);
-    })
-    .then(function(html) {
-      console.log(html);
-      assert.typeOf(html, 'string');
-    });
-  });
+  // it("Is able to fetch from pollcaster", function() {
+  //   return embed
+  //     .fetch("https://www.pollcaster.com/c/poll/4xi4zsfv5gSXhnkaVoT9pfTOOgm")
+  //     .then(function(data) {
+  //       console.log(data);
+  //       console.log("----------------------------------------");
+  //       return embed.render(data);
+  //     })
+  //     .then(function(html) {
+  //       console.log(html);
+  //       assert.typeOf(html, "string");
+  //     });
+  // });
   /*it('Is able to fetch from Graphiq', function() {
     return embed.fetch('https://www.graphiq.com/wlp/dkAYMGlYKlD')
     .then(function(data) {
@@ -167,5 +180,4 @@ describe('Fetch Method', function() {
   //     assert.typeOf(html, 'string');
   //   });
   // });
-
 });
